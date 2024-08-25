cc66207cc66348d3a4ea5cc98d39c793aa65c304
kalpak nemade <61384122+mrkalpak@users.noreply.github.com>
2024-08-25 13:46:28 +0530

    changes

--- server.js
+++ server.js
@@ -7,17 +7,29 @@ const commentsRoutes = require('./routes/commentsRoutes');
 const path = require('path');
 const cors = require('cors');
 const Post = require('./models/Post');
+
 dotenv.config();
 connectDB();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// API Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/posts', postRoutes);
 app.use('/api/comments', commentsRoutes); // Use the comments router
 
+// Serve static files from the 'uploads' directory
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+// Serve static files from the React app's build directory
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Catch-all handler to serve the React app for any route not covered by API routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 
